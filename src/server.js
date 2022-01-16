50ccb7bc149ec4c22b60caac187b89226bb4e846
saracalihan <saracalihan@gmail.com>
2022-01-16 17:07:43 +0300

    join and create room page added, embeded youtube iframe added then simple pause&play action

--- src/server.js
+++ src/server.js
@@ -7,10 +7,11 @@ var ejs = require('ejs');
 
 
 const app = express();
-app.engine('html', ejs.renderFile);
-app.set('views',__dirname.replace('src', '')+'public');
+//app.engine('html', ejs.renderFile);
+//app.set('views',__dirname.replace('src', '')+'public');
+app.use(express.static('public'))
 app.use(cors());
-app.use(express.static(__dirname.replace('src', '')+'public'))
+//app.use(express.static(__dirname.replace('src', '')+'public'))
 app.use(bodyParser.json())
 
 const server = http.createServer(app);
@@ -25,11 +26,16 @@ io.listen();
 // Routes
 
 app.get('', (req, res) => {
-  res.redirect('/login');
+  res.redirect('/create-room');
 });
 
-app.get('/login', (req, res) => {
-  res.sendFile(__dirname.replace('src', '') + 'public/login.html');
+app.get('/create-room', (req, res) => {
+  res.sendFile(__dirname.replace('src', '') + 'public/CreateRoom.html');
+});
+
+app.get('/join-room/:id', (req, res) => {
+  const room = io.getRoom(req.params.id)
+  return res.redirect(`http://localhost:3000/JoinRoom.html?video=${room.video}&roomId=${room.id}`);
 });
 
 app.post('/login', (req, res) => {
--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,21 @@
+require('colors');
+const { randomInt } = require("crypto");
 const { Server } = require("socket.io");
+
+const logToServer = (status, message, date = new Date()) => {
+  status = status.toUpperCase();
+  switch (status) {
+    case 'ROOM':
+      status = status.bgRed;
+      break;
+    case 'MESSAGE':
+      status = status.bgGreen;
+      break;
+    default:
+      status = status.bgBlue;
+  }
+  console.log(`${date.toLocaleString().yellow} - ${status}: ${message}`);
+};
 
 class Socket {
   constructor(server) {
@@ -6,24 +23,23 @@ class Socket {
     this.users = [];
     this.rooms = [];
   }
-  createRoom(roomId, user) {
+  createRoom(video, user) {
     const room = {
-      id: roomId,
+      id: Date.now() + randomInt(9999),
+      video,
       users: [
         user,
       ],
       messages: []
     };
     this.rooms.push(room)
-    console.log({ room });
     return room;
   }
   joinRoom(roomId, user) {
-    if (this.rooms.filter(r => r.id == roomId).length === 0) {
+    if (!this.getRoom(roomId)) {
       return;
     }
     this.rooms[this.rooms.findIndex(r => r.id == roomId)].users.push(user);
-    console.log({ rooms: this.rooms });
   }
   addUser(username, socket) {
     let user = { id: socket.id, username };
@@ -32,6 +48,9 @@ class Socket {
   }
   getUser(id) {
     return this.users.filter(u => u.id == id)[0];
+  }
+  getRoom(id) {
+    return this.rooms[this.rooms.findIndex(r => r.id == id)]
   }
   addMessages(roomId, user, text, isSystemMessage = false) {
     let message = {
@@ -43,27 +62,41 @@ class Socket {
     this.rooms[this.rooms.findIndex(r => r.id == roomId)].messages.push(message);
     return message;
   }
+
+  broadcastEvent(socket, event, ...args) { // io içindeki bazı clients id'leri çözülemeyen bir nedenle string olmadığı için socket.brodacast.to çalışmıyor. 
+    this.io.sockets.sockets.forEach(s => {
+      if (s.id === socket.id) {
+        return
+      }
+      s.emit(event, ...args);
+    })
+  }
+
   listen() {
     let that = this;
     this.io.on('connection', function connection(socket) {
-      socket.on('create-room', ({ username, roomId }) => {
-        const room = that.createRoom(roomId, that.addUser(username, socket));
-        socket.join(roomId);
-        console.log(`ROOM: ${room.id} created by ${room.users[0].username}`);
+      socket.on('create-room', ({ username, video }) => {
+        const room = that.createRoom(video, that.addUser(username, socket));
+        socket.join(room.id);
+        logToServer('room', `${room.id} created by ${room.users[0].username}`);
+        socket.emit('room-info', room);
       });
 
       socket.on('join-room', ({ username, roomId }) => {
+        if (!that.getRoom(roomId)) {
+          return socket.disconnect();
+        }
+
         let user = that.addUser(username, socket);
         that.joinRoom(roomId, user);
         socket.join(roomId);
         that.sendAll(roomId, socket, `${username} joined`, true);
-        console.log(`ROOM: ${user.username} joined to room ${roomId}`);
+        logToServer('room', `${user.username} joined to room ${roomId}`);
       });
-
 
       socket.on('message', function message({ roomId, user, message }) {
         if (message == '!q') {
-          that.sendAll(roomId, socket, `${user} quit`,true);
+          that.sendAll(roomId, socket, `${user} quit`, true);
           return socket.disconnect();
         }
         that.sendAll(roomId, socket, message);
@@ -74,15 +107,30 @@ class Socket {
         socket.emit('room-info', room);
       });
 
-      socket.on('disconnect', (...e) => {
-        console.log({ ...e });
+      socket.on('disconnect', () => {
+      });
+
+      socket.on('durdu', ({ roomId, username }) => {
+        //socket.broadcast.to(roomId).emit('video-durdur');
+        that.broadcastEvent(socket, 'video-durdur');
+        that.sendAll(roomId, socket, `${username} stoped the video`, true);
+      });
+
+      socket.on('oynat', ({ roomId, username }) => {
+        that.broadcastEvent(socket, 'video-oynat');
+        //socket.broadcast.to(roomId).emit('video-oynat');
+        that.sendAll(roomId, socket, `${username} played the video`, true);
       });
     });
   }
 
   sendAll(roomId, socket, message, isSystemMessage = false) {
-    const m = this.addMessages(roomId, socket ? this.getUser(socket.id).username : null, message, isSystemMessage)
-    socket.broadcast.to(roomId).emit('message', JSON.stringify(m))
+    let user = socket ? this.getUser(socket.id).username : null
+    const m = this.addMessages(roomId, user, message, isSystemMessage)
+    //this.io.sockets.sockets.forEach(s => s.emit('message', JSON.stringify(m)))
+    this.broadcastEvent(socket, 'message', JSON.stringify(m))
+    //socket.broadcast.to(roomId).emit('message', JSON.stringify(m));
+    logToServer('message', `${user} send message to room ${roomId}`);
   }
 }
 
